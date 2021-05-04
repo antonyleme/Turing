5f609ec70420f3087d062302c4a73ca112d74854
Antony Leme <antony040402@gmail.com>
2021-05-04 00:48:07 -0300

    offer margin

--- src/styles/components/Offers/Offer/Wrapper.js
+++ src/styles/components/Offers/Offer/Wrapper.js
@@ -2,6 +2,11 @@ import styled from 'styled-components'
 
 const Wrapper = styled.div`
   position: relative;
+
+  @media (max-width: 1024px){
+    padding-bottom: 15px;
+    margin-bottom: 30px;
+  }
 `
 
 export default Wrapper
