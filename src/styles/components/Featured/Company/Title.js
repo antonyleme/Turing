08c3b32410560f330ec564016437e5d4d80cc190
Antony Leme <antony040402@gmail.com>
2021-05-04 00:57:01 -0300

    featured font size

--- src/styles/components/Featured/Company/Title.js
+++ src/styles/components/Featured/Company/Title.js
@@ -2,10 +2,11 @@ import styled from 'styled-components'
 
 const Grid = styled.h3`
   color: black;
-  font-size: 24px;
-  line-height: 32px;
+  font-size: 20px;
+  line-height: 25px;
   font-weight: bold;
   text-align: center;
+  white-space: nowrap;
 `
 
 export default Grid
